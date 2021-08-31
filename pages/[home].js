b60d54526fde22bec4344da96c49c29d4df52b23
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-08-31 23:13:36 +0900

    middle_shizu

--- pages/[home].js
+++ pages/[home].js
@@ -281,7 +281,7 @@ export default function Home({ id, database }) {
           <h3>{userData?.name}</h3>
           <p className={styles.accountTextJob}>{userData?.job}</p>
           <p className={styles.accountTextBio}>{userData?.bio}</p>
-          <a href="instagram://user?username=saikai_official">リンク</a>
+          <a href="instagram://user?username=middle_shizu">リンク</a>
         </div>
       </div>
 
