98b5fc49769a3e23fcc90422ea53e7e91b7ac6ff
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-09-02 18:35:46 +0900

    test01

--- pages/test/[userid].js
+++ pages/test/[userid].js
@@ -29,14 +29,14 @@ const getData = async (user) => {
 
   const getUserData = async (u) => {
     await u.forEach(async (us) => {
-      userId = us.id;
+      userId = us?.id;
       userObj = us.data();
     });
   };
   const getPlaceData = async (p) => {
     p.forEach(async (place) => {
       placeObj.place[place.data().month].push({
-        ...{ id: place.id },
+        ...{ id: place?.id },
         ...place.data(),
       });
     });
@@ -143,107 +143,13 @@ export default function Home({ id, database }) {
     getProfileImg();
   }, []);
 
-  // いきたいところコンポーネント
-  const PlaceSetBoxs = () => {
-    let monthComps = {};
-    for (let i = 0; i < 12; i++) {
-      if (placeData[monthes[i]].length !== 0) {
-        monthComps[monthes[i]] = placeData[monthes[i]].map((p, i) => {
-          return (
-            <li key={i} className={styles.placeListBox}>
-              <div className={styles.emoji}>
-                {String.fromCodePoint(p?.emoji)}
-              </div>
-              <div className={styles.placeTextBox}>
-                <div className={styles.name}>{p?.name}</div>
-              </div>
-              <div className={styles.placeBtnBox}>
-                <button
-                  className={styles.placeGo}
-                  onClick={() => goPlace(p?.id)}
-                >
-                  いきたい！
-                </button>
-                <button
-                  className={styles.placeDelete}
-                  onClick={() => deletePlace(p?.id)}
-                >
-                  削除
-                </button>
-              </div>
-            </li>
-          );
-        });
-      }
-    }
-    let ulComps = [];
-    for (let i = 0; i < Object.keys(monthComps).length; i++) {
-      let key = Object.keys(monthComps)[i];
-      ulComps.push(
-        <ul key={i} className={styles.placeUlBox + " " + styles[key]}>
-          <h3>{String.fromCodePoint(monthEmoji[key]) + " " + key}</h3>
-          {monthComps[key]}
-        </ul>
-      );
-    }
-    return ulComps;
-  };
-
-  // いきたい場所を追加
-  const addPlace = async () => {
-    const place = document.getElementById("addPlaceText").value;
-    const placeObj = {
-      name: place,
-      emoji: "0x1F37B",
-    };
-    if (place !== "") {
-      await db
-        .collection("users")
-        .doc(userData.userId)
-        .collection("place")
-        .add(placeObj);
-      setPlaceData([...placeData, placeObj]);
-    }
-    document.getElementById("addPlaceText").value = "";
-  };
-
-  // いきたい場所を削除
-  const deletePlace = async (id) => {
-    await db
-      .collection("users")
-      .doc(userData.userId)
-      .collection("place")
-      .doc(id)
-      .delete();
-    setPlaceData(
-      placeData.filter((item) => {
-        return item.id !== id;
-      })
-    );
-  };
-
-  const goPlace = async (id) => {
-    console.log("go : " + id);
-    document.getElementById("modal").style.display = "flex";
-  };
-  const closeModal = () => {
-    modal.style.display = "none";
-  };
-  if (typeof window !== "undefined") {
-    window.onclick = function (event) {
-      if (event.target == document.getElementById("modal")) {
-        document.getElementById("modal").style.display = "none";
-      }
-    };
-  }
-
   return (
     <div className={styles.container}>
       <Head>
-        <title>{userData.id}</title>
+        <title>{userData?.id}</title>
         <meta
           name="description"
-          content={userData.id + "を気軽に誘っちゃおう"}
+          content={userData?.id + "を気軽に誘っちゃおう"}
         />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:image" content="test" />
