8f17c54484379854410e352daf9495df854a2ccc
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-09-07 04:11:26 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import "../styles/globals.scss";
 import "../styles/reset.scss";
 import AuthProvider from "../lib/auth";
 
+export function reportWebVitals(metric) {
+  console.log("パフォーマンス", metric.name, metric.value);
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
