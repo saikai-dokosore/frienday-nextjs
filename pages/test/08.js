ed3bb4141c7715efb14ce35a6ddddf0c68e78103
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-09-10 14:05:37 +0900

    test

---
+++ pages/test/08.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/Test.module.scss";
+
+export default function Index() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>home</title>
+        <meta name="description" content="test" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="test" />
+        <meta name="og:title" content="test" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/images/08.png" />
+      </main>
+    </div>
+  );
+}
--- pages/test/admin.js
+++ pages/test/admin.js
@@ -17,32 +17,20 @@ export default function Index() {
       <main className={styles.admin}>
         <h1>DEMO PAGE</h1>
         <div>
-          <Link href="/test/01">
-            <a>デモ01</a>
-          </Link>
-          <Link href="/test/02">
-            <a>デモ02</a>
-          </Link>
-          <Link href="/test/03">
-            <a>デモ03</a>
-          </Link>
-          <Link href="/test/04">
-            <a>デモ04</a>
-          </Link>
           <Link href="/test/05">
             <a>デモ05</a>
-          </Link>
-          <Link href="/test/06">
-            <a>デモ06</a>
           </Link>
           <Link href="/test/07">
             <a>デモ07</a>
           </Link>
-          <Link href="/saikai_official">
-            <a>saikai_official</a>
+          <Link href="/test/08">
+            <a>デモ08</a>
           </Link>
           <Link href="/test/design">
-            <a>design</a>
+            <a>デモ09</a>
+          </Link>
+          <Link href="/saikai_official">
+            <a>saikai_official</a>
           </Link>
           <Link href="https://vsco.co/saikai0011">
             <a>VSCO</a>
