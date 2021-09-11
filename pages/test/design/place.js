a0fc7a9485225495d5ad108c009b216ea331410b
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-09-11 17:33:14 +0900

    test

--- pages/test/design/place.js
+++ pages/test/design/place.js
@@ -26,6 +26,9 @@ export default function Index() {
           width={100}
           height={100}
         />
+        <Link href={"instagram://tag?name=andpeople"}>
+          <a>#andpeople</a>
+        </Link>
         <p>あなたはグッドされていません</p>
         <button onClick={() => {}}>いきたい！</button>
       </div>
